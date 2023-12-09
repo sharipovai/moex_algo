592c3159e908e8b3e781c6586f9a4c7cfc93a508
sharipov <sharipov@dedicorp.ru>
2023-12-09 15:19:41 +0300

    add json data and appotunity to choose company name

--- alga_bot.py
+++ alga_bot.py
@@ -1,14 +1,14 @@
 import os
-
+import json
 import telebot
 
 from telebot import types
 from algorithms import get_prediction
 
-bot = telebot.TeleBot('6504447282:AAH2b86-jWK3MehFQHDb-Li6whYu-TUewVM')
+bot = telebot.TeleBot('6986377523:AAFIcc-iDKEnE6S-r0lP-XQSvHkCXuBOjvQ')
 
 mode_type=0
-trade_code = ''
+company_data = []
 
 
 @bot.message_handler(commands=["start"])
@@ -20,7 +20,7 @@ def start(message):
     markup.add(but1)
     markup.add(but2)
     markup.add(but3)
-    hello_mess = f"Привет, {message.from_user.first_name}! Меня зовут Nala, я искусственный интеллект, созданный для помощи инвесторам и трейдерам на московской бирже."
+    hello_mess = f"Привет, {message.from_user.first_name}!\n"
     bot.send_message(message.chat.id, hello_mess, parse_mode='html', reply_markup=markup)
     bot.send_message(message.chat.id,
                      f"Существует три основных вида торговли на бирже в зависимости от времени, которое проходит между покупкой и продажей ценных бумаг:\n"
@@ -36,44 +36,54 @@ def on_click(message):
             'Среднесрочный': 1,
             'Долгосрочный': 2}
     if message.text in mode.keys():
-        bot.send_message(message.chat.id, "Отлично! Теперь введи тикер, по которому ты хочешь получить информацию",
+        bot.send_message(message.chat.id, "Отлично! Теперь введите тикер, или название компании, для которой Вы хотите получить информацию. "
+                                          "Например: SBER (Сбербанк), LKOH (Лукойл), GAZP (Газпром)...",
                          parse_mode='html')
         global mode_type
         mode_type = mode[message.text]
         bot.register_next_step_handler(message, get_company_for_trade)
     else:
         bot.send_message(message.chat.id,
-                         f"Нужно выбрать один из трех режимов, попробуй еще раз")
+                         f"Нужно выбрать один из трех режимов, попробуйте еще раз.")
         bot.register_next_step_handler(message, on_click)
 
 
 @bot.message_handler(content_types=["text"])
 def get_company_for_trade(message):
     if message.text:
-        with open("save_tickers.txt", "r") as file:
-            lines = file.readlines()
-
-        is_company_exist = False
-
-        for line in lines:
-            if f"'{message.text.strip().upper()}'," in line:
-                is_company_exist = True
-                break
-
-        if is_company_exist:
+        global company_data
+        company_data = get_company_or_tiket_data(message.text)
+        if company_data:
+            bot.send_message(message.chat.id, f"Было выбрано: {company_data} \n")
             bot.send_message(message.chat.id, "Провожу анализ ценных бумаг...")
-            global trade_code
-            trade_code = message.text.strip().upper()
-            get_predict(message)
-            bot.register_next_step_handler(message, on_click)
+            get_predict(message, company_data[0])
+            bot.register_next_step_handler(message, start)
         else:
             bot.send_message(message.chat.id,
-                             f"Компания {message.text.strip()} пока недоступна. Попробуйте другой тикер")
+                             f"Тикет/компания {message.text.strip()} пока недоступа. Попробуйте ввести название на другом языке."
+                             f"Например: SBER (Сбербанк), LKOH (Лукойл), GAZP (Газпром)...")
             bot.register_next_step_handler(message, get_company_for_trade)
 
+def get_company_or_tiket_data(search_text):
+    #return list [trade_code, INSTRUMENT_TYPE, EMITENT_FULL_NAME]
+    trade_code = ''
+    with open('trade_data.json', 'r', encoding='utf-8') as file:
+        trade_data = json.load(file)
+        if search_text.strip().upper() in trade_data.keys():
+            trade_code = search_text.strip().upper()
+        else:
+            for key in trade_data.keys():
+                if search_text.strip().upper() in trade_data[key]['EMITENT_FULL_NAME'].upper():
+                    trade_code = key
+                    break
+    if trade_code != '':
+        company_data = [trade_code, trade_data[trade_code]['INSTRUMENT_TYPE'], trade_data[trade_code]['EMITENT_FULL_NAME']]
+    else:
+        company_data = []
+    return company_data
 
-def get_predict(message):
-    global mode_type, trade_code
+def get_predict(message, trade_code):
+    global mode_type
     predict = get_prediction(trade_code, mode_type, message.from_user.id)
     if predict in [0, 1]:
         image_path='images/'+str(message.from_user.id)+'.jpg'
